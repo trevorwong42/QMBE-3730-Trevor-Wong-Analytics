29efb6c33f6dbeac7720df955e76fc48d0e296b4
trevorwong42 <twong03@hamline.edu>
2024-01-26 00:29:10 -0600

    Assignment 0 - Trevor Wong
    WIP

---
+++ Assignment 0-QMBE 3730.R
@@ -0,0 +1,22 @@
+#What is the shap of the data set
+creditdata <- read.csv("G:\\My Drive\\2024 Spring\\QMBE 3730 Analytic\\loan_default_data_set.csv")
+
+
+#What are the column names
+
+
+#What are the data types
+
+
+#How much of my data set is missing
+sum(is.na(creditdata))
+
+
+#What initial story does the data convey
+#this data looks at a number of variables and reports if the account is in default. An example of these variable is 
+
+
+#Summary statistics(mean,variance,max,SD)
+
+
+
