f95dad20f0002900a2e70af5d8fc203f7b5abb03
trevorwong42 <twong03@hamline.edu>
2024-01-28 14:36:01 -0600

    Complete Assignment

--- Assignment 0-QMBE 3730.R
+++ Assignment 0-QMBE 3730.R
@@ -1,15 +1,19 @@
-#What is the shap of the data set
+#how big is my data set?
 creditdata <- read.csv("G:\\My Drive\\2024 Spring\\QMBE 3730 Analytic\\loan_default_data_set.csv")
+#20000 observations of 21 variables
 
 
 #What are the column names
 ncol(creditdata)
 head(creditdata)
 
+
 #What are the data types
 # the data varies. There is both Numerical data and categorical data.
 #for example tot_bal is numerical continuous because it can be any value of money the borrow has
 #Def_ind on the other hand is categorical nominal because, while the value is a number, it is indicating if the borrower is in default or not. A mutually exclusive category
+#Also the education is categorical ordinal because they are not mutually exclusive
+
 
 #How much of my data set is missing
 sum(is.na(creditdata))
@@ -26,4 +30,5 @@ mean(creditdata$tot_balance, na.rm=TRUE)
 max(creditdata$tot_balance, na.rm=TRUE)
 # this is saying the max of total balance is 2e+05, this seems absurdly high possibly an error
 var(creditdata$tot_bal,creditdata$def_ind)
-sd(creditdata$def_ind, na.rm=TRUE)
+#this measures how far each observation is from the mean
+sd(creditdata$tot_balance, na.rm=TRUE)
